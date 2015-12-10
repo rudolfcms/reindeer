4cefa6666e992f1136d4510c33b44f4689345848
Mikołaj Pich <m.pich@outlook.com>
2015-12-10 22:19:55 +0100

    Rename file and add class with theme config

---
+++ reindeer.php
@@ -0,0 +1,27 @@
+<?php
+
+class Reindeer {
+	public function __construct() {
+		$this->article = [
+			'thumb' => [
+				'default' => 'images/reindeer.png', // https://pixabay.com/p-296524/?no_redirect
+				'width' => 1000,
+				'height' => 300
+			],
+			'date' => [
+				'default' => [
+					'locale' => '%e %B %Y '. _('date_on') .' %H:%M', // http://sjp.pwn.pl/poradnia/haslo/;10340
+					'normal' => 'Y-m-d H:i:s'
+				]
+			],
+			'pagination' => [
+				'onPage' => 15,
+				'navNumber' => 7,
+				'classes' => [
+					'ul'=>'cd-pagination', 
+					'current'=>'current'
+				]
+			]
+		];
+	}
+}
